b5b8b6244d44cdd7796cc79cb5689039d6406fb0
Maciej Matczak <maciek.matczak@gmail.com>
2019-07-26 19:17:57 +0200

    Reimplement drill verification into reusable function
    Add 2 more scenarios for drill checking.

--- ergo42_pcb_case/tasks.py
+++ ergo42_pcb_case/tasks.py
@@ -3,9 +3,13 @@ import gerber
 from textwrap import dedent
 
 
-BOARD_DRILLS = '../ergo42/ergo42/ergo42.drl'
-COVER_DRILLS = '../ergo42_pcb_case/cover/gerbers/cover.drl'
-PLATE_DRILLS = '../ergo42_pcb_case/plate/gerbers/plate.drl'
+MOUNTING_HOLE_PRETTY = '/usr/share/kicad/modules/MountingHole.pretty'
+MOUNTING_HOLE_NAME = 'MountingHole_2.2mm_M2_Pad'
+
+BOARD_DRILL_FILE = '../ergo42/ergo42/ergo42.drl'
+PLATE_DRILL_FILE = '../ergo42_pcb_case/plate/gerbers/plate.drl'
+BOTTOM_DRILL_FILE = '../ergo42_pcb_case/bottom/gerbers/bottom.drl'
+COVER_DRILL_FILE = '../ergo42_pcb_case/cover/gerbers/cover.drl'
 
 PROJECTS = ['bottom', 'cover', 'plate']
 PLOTS = 'gerbers'
@@ -35,13 +39,18 @@ def get_drills_of_dia(drill_file_path, diameter, normalize=True):
             tool = drills.tools[t]
 
     if not tool:
-        raise Exit('Standoff tool not found!')
+        raise Exit(f'Tool for diameter {diameter} not found!')
 
     hits = [hit for hit in drills.hits if hit.tool is tool]
     for hit in hits:
         hit.to_metric()
+
     coords = [hit.position for hit in hits]
 
+    return normalize_coords(coords)
+
+
+def normalize_coords(coords):
     origin = coords[0]
     for hole in coords:
         origin = min(origin, hole)
@@ -49,26 +58,101 @@ def get_drills_of_dia(drill_file_path, diameter, normalize=True):
     xn, yn = origin
     for i, _ in enumerate(coords):
         x, y = coords[i]
-        coords[i] = (x - xn, y - yn)
 
-    return coords
+        # hard coding decimal place precision
+        coords[i] = (
+            round(x - xn, 6),
+            round(y - yn, 6)
+        )
+
+    return sorted(coords)
+
+
+def verify_drills(spec):
+    designs_holes = {}
+
+    for slug, data in spec.items():
+        holes = get_drills_of_dia(data['drill_file'], data['tool_diameter'])
+
+        if 'filter_out_f' in data:
+            holes = [h for h in holes if not data['filter_out_f'](h)]
+            if holes[0] != (0, 0):
+                holes = normalize_coords(holes)
+
+        if len(holes) != data['holes_number']:
+            raise Exit(dedent(f"""\
+                Found {len(holes)} holes in {slug}, expected {data["holes_number"]}:
+                {holes}
+            """))
+
+        designs_holes[slug] = sorted(holes)
+
+    a, b = designs_holes.keys()
+    if designs_holes[a] != designs_holes[b]:
+        raise Exit(dedent(f"""\
+            Holes did not match:
+            {a} = {designs_holes[a]}
+            {b} = {designs_holes[b]}
+        """))
+
+    print('PASS!')
 
 
 @task()
 def verify(c):
-    board_standoff_holes = get_drills_of_dia(BOARD_DRILLS, 5)
+    print('Checking board vs plate drills...')
+    spec = {
+        'board': {
+            'tool_diameter': 5,
+            'holes_number': 5,
+            'drill_file': BOARD_DRILL_FILE
+        },
+        'plate': {
+            'tool_diameter': 2.2,
+            'holes_number': 5,
+            'drill_file': PLATE_DRILL_FILE
+        }
+    }
+    verify_drills(spec)
 
-    if len(board_standoff_holes) != 5:
-        raise Exit(f'Found {len(board_standoff_holes)} holes in board, expected 5')
+    print('Checking board vs bottom drills...')
+    spec = {
+        'board': {
+            'tool_diameter': 5,
+            'holes_number': 5,
+            'drill_file': BOARD_DRILL_FILE
+        },
+        'bottom': {
+            'tool_diameter': 2.2,
+            'holes_number': 5,
+            'drill_file': BOTTOM_DRILL_FILE,
+            'filter_out_f': lambda xy: xy[1] > 60  # holes above 60 are for cover
+        }
+    }
+    verify_drills(spec)
 
-    plate_standoff_holes = get_drills_of_dia(PLATE_DRILLS, 2.35)
+    print('Checking bottom vs cover drills...')
+    spec = {
+        'bottom': {
+            'tool_diameter': 2.2,
+            'holes_number': 4,
+            'drill_file': BOTTOM_DRILL_FILE,
+            'filter_out_f': lambda xy: xy[1] < 60  # holes above 60 are for cover
+        },
+        'cover': {
+            'tool_diameter': 2.2,
+            'holes_number': 4,
+            'drill_file': COVER_DRILL_FILE,
+            'filter_out_f': lambda xy: 60 < xy[0] < 62  # middle cover hole for reset
+        }
+    }
+    verify_drills(spec)
 
-    if len(plate_standoff_holes) != 5:
-        raise Exit(f'Found {len(board_standoff_holes)} holes in cover, expected 5')
 
-    if sorted(board_standoff_holes) != sorted(plate_standoff_holes):
-        raise Exit(dedent(f"""\
-            Holes did not match:
-            board = {sorted(board_standoff_holes)}
-            plate = {sorted(plate_standoff_holes)}
-        """))
+@task()
+def circles2holes(c):
+    for project in PROJECTS:
+        c.run(
+            f'python scripts/circles2holes.py {project}/{project}.kicad_pcb \
+                {MOUNTING_HOLE_PRETTY} {MOUNTING_HOLE_NAME}'
+        )
